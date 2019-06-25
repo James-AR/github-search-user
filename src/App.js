d9c3e3a1bf3ff21535922656582a6b3cafce2ef7
James <ruiz.james12@yahoo.com>
2019-06-25 12:30:56 -0700

    Search form

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import Navbar from './components/layout/Navbar'
 import Users from './components/users/Users'
+import Search from './components/users/Search'
 import axios from 'axios'
 
 class App extends Component {
@@ -15,7 +16,7 @@ class App extends Component {
 
    const res = await axios.get(`https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`)
 
-   this.setState({ users: res.data, laoding: false })
+   this.setState({ users: res.data, loading: false })
    console.log(this.state.users)
   }
 
@@ -26,6 +27,7 @@ class App extends Component {
       <div className="App">
         <Navbar />
         <div className="container">
+          <Search />
           <Users loading={loading} users={users} />
         </div>
       </div>

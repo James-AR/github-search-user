b8dd74ba56613ecc722bb2b7e7c37b3a224ebf02
James <ruiz.james12@yahoo.com>
2019-06-25 13:07:31 -0700

    Search users functionality

--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -9,13 +9,14 @@ export class Search extends Component {
     
     onSubmit = (e) => {
         e.preventDefault()
-        console.log(this.state.text)
+        this.props.searchUsers(this.state.text)
+        this.setState({ text: '' })
     }
 
     render() {
         return (
             <div>
-                <form onSubmit={this.Submit}className="form">
+                <form onSubmit={this.onSubmit} className="form">
                     <input type="text" name="text" placeholder="Search Users.." value={this.state.text} onChange={this.onChange}/>
                     <input type="submit" value="Search" className="btn btn-dark btn-block" />
                 </form>

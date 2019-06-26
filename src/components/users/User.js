258c259e6a583a5a89563ba6e4e25e3ee37b79cf
James <ruiz.james12@yahoo.com>
2019-06-26 13:56:09 -0700

    User.js rendering avatar, name, etc

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -1,4 +1,5 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react'
+import { Link } from 'react-router-dom'
 
 export class User extends Component {
     componentDidMount() {
@@ -9,9 +10,27 @@ export class User extends Component {
         const { name, avatar_url, location, bio, blog, login, html_url, followers, following, public_repos, title } = this.props.user
 
         return (
-            <div>
-                { name }
-            </div>
+            <Fragment>
+                <Link to='/' className='btn btn-light' >Back to Search</Link>
+                <div className='card grid-2'>
+                    <div className='all-center'>
+                        <img src={avatar_url} className='round-img' alt='' style={{ width: '150px' }} />
+                        <h1>{name}</h1>
+                        <p>Location: {location}</p>
+                    </div>
+                    <div>
+                        {bio && (
+                        <Fragment>
+                            <h3>Bio</h3>
+                            <p>{bio}</p>
+                        </Fragment>
+                        )}
+                        <a href={html_url} className='btn btn-dark my-1'>
+                            Visit Github Profile
+                        </a>
+                    </div>
+                </div>
+            </Fragment>
         )
     }
 }
